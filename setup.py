4d9f935f89f594b83de51602b2af942ae8da414a
Livia Pereira <livaper@gmail.com>
2017-10-17 06:34:52 -0200

    Adding configuration files to make the project a library

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = "Epoca Cosmeticos Web Crawler",
+    version = "0.1",
+    packages = ['epoca_cosmeticos_web_crawler', 'tests'],
+
+    # metadata
+    author = "Livia Costa Pereira",
+    author_email = "livaper@gmail.com",
+    description = "Library to crawl the website epocacosmeticos.com.br"
+                  "and list all product's name, title and url in a csv",
+)
